2650b9f431377c6ac353aee036e7f87b76627f29
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2024-12-27 21:01:17 +0100

    Gen TS code

--- schema/out/@typespec/json-schema/ts/ifc5file.d.ts
+++ schema/out/@typespec/json-schema/ts/ifc5file.d.ts
@@ -9,37 +9,125 @@ export type Ifc5FileJson = (ClassJson | DefJson | OverJson | DisclaimerJson)[];
 
 export interface ClassJson {
   def: "class";
-  type?: "UsdGeom:Mesh" | "UsdGeom:Xform" | "UsdGeom:BasisCurves" | "UsdShade:Material" | "UsdShade:Shader";
+  type?: "UsdGeom:Mesh" | "UsdGeom:Xform" | "UsdGeom:BasisCurves" | "UsdShade:Material" | "UsdShade:Shader" | "Xform";
   name: string;
   children?: DefJson[];
 }
 export interface DefJson {
   def: "def";
-  type?: "UsdGeom:Mesh" | "UsdGeom:Xform" | "UsdGeom:BasisCurves" | "UsdShade:Material" | "UsdShade:Shader";
+  type?: "UsdGeom:Mesh" | "UsdGeom:Xform" | "UsdGeom:BasisCurves" | "UsdShade:Material" | "UsdShade:Shader" | "Xform";
   name: string;
   children?: DefJson[];
 }
 export interface OverJson {
   def: "over";
   name: string;
-  attributes2: {
-    "UsdGeom:Mesh"?: UsdGeomMeshJson;
-    "UsdGeom:VisibilityAPI:visibility"?: UsdGeomVisibilityAPIvisibilityJson;
+  attributes:
+    | Ifc5AlignmentsegmentComponentJson
+    | Ifc5AlignmentComponentJson
+    | UsdShadeMaterialComponentJson
+    | Ifc5SpaceboundaryComponentJson
+    | NLSFBComponentJson
+    | UsdGeomBasisCurvesComponentJson
+    | PropertiesComponentJson
+    | CustomDataComponentJson
+    | XformOpComponentJson
+    | UsdShadeMaterialBindingAPIComponentJson
+    | UsdGeomMeshComponentJson
+    | UsdGeomVisibilityAPIvisibilityComponentJson
+    | Ifc5ClassComponentJson;
+}
+export interface Ifc5AlignmentsegmentComponentJson {
+  "ifc5:alignmentsegment":
+    | {
+        EndRadiusOfCurvature: number;
+        PredefinedType: "LINE" | "CLOTHOID" | "CIRCULARARC" | "CONSTANTGRADIENT";
+        SegmentLength: number;
+        StartDirection: number;
+        StartPoint: number[];
+        StartRadiusOfCurvature: number;
+      }
+    | {
+        EndGradient: number;
+        PredefinedType: "LINE" | "CLOTHOID" | "CIRCULARARC" | "CONSTANTGRADIENT";
+        HorizontalLength: number;
+        StartDistAlong: number;
+        StartGradient: number;
+        StartHeight: number;
+      }
+    | {
+        EndCantLeft: number;
+        EndCantRight: number;
+        HorizontalLength: number;
+        PredefinedType: "CONSTANTCANT" | "LINEARTRANSITION";
+        StartCantLeft: number;
+        StartCantRight: number;
+        StartDistAlong: number;
+      };
+}
+export interface Ifc5AlignmentComponentJson {
+  "ifc5:alignment": {
+    segments: {
+      ref: string;
+    }[];
   };
-  attributes: UsdGeomMeshComponentJson | UsdGeomVisibilityAPIvisibilityComponentJson;
 }
-export interface UsdGeomMeshJson {
-  faceVertexIndices: number[];
-  points: number[][];
+export interface UsdShadeMaterialComponentJson {
+  "UsdShade:Material": {
+    "outputs:surface.connect": {
+      ref: string;
+    };
+  };
 }
-export interface UsdGeomVisibilityAPIvisibilityJson {
-  visibility: "visible" | "invisible";
+export interface Ifc5SpaceboundaryComponentJson {
+  "ifc5:spaceboundary": {
+    relatedElement: {
+      ref: string;
+    };
+  };
+}
+export interface NLSFBComponentJson {
+  "nlsfb:class": unknown;
+}
+export interface UsdGeomBasisCurvesComponentJson {
+  "UsdGeom:BasisCurves": {
+    points: number[][];
+  };
+}
+export interface PropertiesComponentJson {
+  "ifc5:properties": unknown;
+}
+export interface CustomDataComponentJson {
+  customdata: unknown;
+}
+export interface XformOpComponentJson {
+  xformOp: {
+    transform: number[][];
+  };
+}
+export interface UsdShadeMaterialBindingAPIComponentJson {
+  "UsdShade:MaterialBindingAPI": {
+    "material:binding": {
+      ref: string;
+    };
+  };
 }
 export interface UsdGeomMeshComponentJson {
-  "UsdGeom:Mesh": UsdGeomMeshJson;
+  "UsdGeom:Mesh": {
+    faceVertexIndices: number[];
+    points: number[][];
+  };
 }
 export interface UsdGeomVisibilityAPIvisibilityComponentJson {
-  "UsdGeom:VisibilityAPI:visibility": UsdGeomMeshJson;
+  "UsdGeom:VisibilityAPI:visibility": {
+    visibility: "visible" | "invisible";
+  };
+}
+export interface Ifc5ClassComponentJson {
+  "ifc5:class": {
+    code: string;
+    uri: string;
+  };
 }
 export interface DisclaimerJson {
   disclaimer: string;
