00f89e9c696d57ecce53b773db85af1c8b16df9e
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2025-03-21 15:48:28 +0100

    Diff workflow

--- docs/viewer/compose-alpha.ts
+++ docs/viewer/compose-alpha.ts
@@ -60,6 +60,105 @@ function LoadIfcxFile(file: IfcxFile)
     return ExpandNodeWithInput(file.header.defaultNode, ToInputNodes(file.data));
 }
 
+function MakeInputNode(path: string)
+{
+    return {
+        path,
+        children: {},
+        inherits: {},
+        attributes: {}
+    } as InputNode;
+}
+
+function DeepEqual(a: any, b: any)
+{
+    // TODO: slow
+    return JSON.stringify(a) === JSON.stringify(b);
+}
+
+// Node 2 wins
+function DiffNodes(node1: InputNode, node2: InputNode): IfcxNode
+{
+    let result = {
+        name: node1.path,
+        children: {},
+        inherits: {},
+        attributes: {}
+    } as IfcxNode;
+
+    Object.keys(node1.children).forEach((name) => {
+        if (node1.children[name] !== node2.children[name])
+        {
+            result.children[name] = node2.children[name] ? node2.children[name] : null;
+        }
+    })
+    
+    Object.keys(node1.inherits).forEach((name) => {
+        if (node1.inherits[name] !== node2.inherits[name])
+        {
+            result.inherits[name] = node2.inherits[name] ? node2.inherits[name] : null;
+        }
+    })
+    
+    Object.keys(node1.attributes).forEach((name) => {
+        if (!DeepEqual(node1.attributes[name], node2.attributes[name]))
+        {
+            result.attributes[name] = node2.attributes[name] ? node2.attributes[name] : null;
+        }
+    })
+
+    return result;
+}
+
+// file 2 wins
+function Diff(file1: IfcxFile, file2: IfcxFile)
+{
+    let result: IfcxFile = {
+        header: file1.header,
+        schemas: {},
+        data: []
+    };
+
+    let i1 = ToInputNodes(file1.data);
+    let i2 = ToInputNodes(file2.data);
+
+    for (let [path, nodes] of i1)
+    {
+        let file2Node: InputNode | null = null;
+        if (i2.has(path))
+        {
+            // diff
+            file2Node = Collapse(i2.get(path)!);
+        }
+        if (file2Node === null)
+        {
+            // node was removed, make dummy
+            file2Node = MakeInputNode(path);
+        }
+        let file1Node = Collapse(nodes)!;
+        result.data.push(DiffNodes(file1Node, file2Node));
+    }
+
+    for (let [path, nodes] of i2)
+    {
+        let file1Node: InputNode | null = null;
+        if (i1.has(path))
+        {
+            // diff
+            file1Node = Collapse(i1.get(path)!);
+        }
+        if (file1Node === null)
+        {
+            // node was added, make dummy`
+            file1Node = MakeInputNode(path);
+        }
+        let file2Node = Collapse(nodes)!;
+        result.data.push(DiffNodes(file1Node, file2Node));
+    }
+
+    return result;
+}
+
 function Federate(file1: IfcxFile, file2: IfcxFile)
 {
     let result: IfcxFile = {
@@ -74,7 +173,7 @@ function Federate(file1: IfcxFile, file2: IfcxFile)
     return Prune(result);
 }
 
-function Collapse(nodes: InputNode[]): InputNode
+function Collapse(nodes: InputNode[], deleteEmpty: boolean = false): InputNode | null
 {
     let result: InputNode = {
         path: nodes[0].path,
@@ -95,12 +194,26 @@ function Collapse(nodes: InputNode[]): InputNode
         })
     })
 
-    // even if a node is empty, it sticks around, as it may be doing a delete
+    if (deleteEmpty)
+    {
+        let empty = true;
+        Object.keys(result.children).forEach((name) => {
+            if (result.children[name] !== null) empty = false;
+        })
+        Object.keys(result.inherits).forEach((name) => {
+            if (result.inherits[name] !== null) empty = false;
+        })
+        Object.keys(result.attributes).forEach((name) => {
+            if (result.attributes[name] !== null) empty = false;
+        })
+
+        if (empty) return null;
+    }
 
     return result;
 }
 
-function Prune(file: IfcxFile)
+function Prune(file: IfcxFile, deleteEmpty: boolean = false)
 {
     let result: IfcxFile = {
         header: file.header,
@@ -111,8 +224,8 @@ function Prune(file: IfcxFile)
     let inputNodes = ToInputNodes(file.data);
 
     inputNodes.forEach((nodes) => {
-        let collapsed = Collapse(nodes);
-        result.data.push({
+        let collapsed = Collapse(nodes, deleteEmpty);
+        if (collapsed) result.data.push({
             name: collapsed.path,
             children: collapsed.children,
             inherits: collapsed.inherits,
