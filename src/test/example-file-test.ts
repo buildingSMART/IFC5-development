ff758ad32ad6889ca4e15dbbc98f79e118251009
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2025-06-08 22:16:31 +0200

    Ik wil alleen maar zwemmen

--- src/test/example-file-test.ts
+++ src/test/example-file-test.ts
@@ -2,20 +2,30 @@ import * as fs from "fs";
 
 import { describe, it } from "./util/cappucino";
 import { expect } from "chai";
-import { FetchRemoteSchemas, LoadIfcxFile } from "../ifcx-core/workflows";
+import { LoadIfcxFile } from "../ifcx-core/workflows";
 import { IfcxFile } from "../ifcx-core/schema/schema-helper";
+import { InMemoryLayerProvider, StackedLayerProvider } from "../ifcx-core/project/layer-providers";
+import { FetchLayerProvider } from "../ifcx-core/project/fetch-layer-provider";
+import { IfcxProjectBuilder } from "../ifcx-core/project/project";
 
 let examplesFolderPath = "../examples"
 
 describe("example file", () => {
     it("hello wall validates properly", async () => {
         let file = JSON.parse(fs.readFileSync(`${examplesFolderPath}/Hello Wall/hello-wall.ifcx`).toString()) as IfcxFile;
-        await FetchRemoteSchemas(file);
-        expect(() => LoadIfcxFile(file)).to.not.throw();
+        
+        let provider = new StackedLayerProvider([
+            new InMemoryLayerProvider().AddAll([file]), 
+            new FetchLayerProvider()
+        ]);
+
+        let project = await (new IfcxProjectBuilder(provider).FromId(file.header.id)).Build();
+        console.log(project);
+        expect(project instanceof Error).to.be.false;
     });
     it("signals validates properly", async () => {
         let file = JSON.parse(fs.readFileSync(`${examplesFolderPath}/Linear placement of signals/linear-placement-of-signal.ifcx`).toString()) as IfcxFile;
-        await FetchRemoteSchemas(file);
+        // await FetchRemoteSchemas(file);
         expect(() => LoadIfcxFile(file)).to.not.throw();
     });
 });
