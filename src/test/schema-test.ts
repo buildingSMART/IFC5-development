f65bd43e8424e4096883157ef0ea7d37cfdcb2b7
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2025-06-14 10:53:40 +0200

    Extra test

--- src/test/schema-test.ts
+++ src/test/schema-test.ts
@@ -39,7 +39,8 @@ describe("schemas", () => {
         expect(() => LoadIfcxFile(ExampleFile("example::object", {val1: "", val2: "a"}))).to.not.throw(SchemaValidationError);
         
         expect(() => LoadIfcxFile(ExampleFile("example::optional_object", {val1: "", val2: "a"}))).to.not.throw(SchemaValidationError);
-        expect(() => LoadIfcxFile(ExampleFile("example::optional_object", {val1: null, val2: "a"}))).to.not.throw(SchemaValidationError);
+        expect(() => LoadIfcxFile(ExampleFile("example::optional_object", {val1: undefined, val2: "a"}))).to.not.throw(SchemaValidationError);
+        expect(() => LoadIfcxFile(ExampleFile("example::optional_object", {val2: "a"}))).to.not.throw(SchemaValidationError);
         expect(() => LoadIfcxFile(ExampleFile("example::optional_object", {val1: null, val2: null}))).to.throw(SchemaValidationError);
 
         expect(() => LoadIfcxFile(ExampleFileWithSchema("Array", null))).to.throw(SchemaValidationError);
